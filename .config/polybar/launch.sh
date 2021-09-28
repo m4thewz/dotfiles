9a20b67b6bcafffff808c5a956de246ad2fda0b5
Matheus Vitor <m4theusdev@gmail.com>
2021-09-27 22:05:50 -0300

    :recycle: Updated configuration of polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,32 @@
 #!/usr/bin/env bash
 
-DIR="$HOME/.config/polybar"
 
-# Terminate already running bar instances
-killall -q polybar
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+CARD="$(ls /sys/class/backlight | head -n 1)"
+RFILE="$DIR/.module"
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Fix backlight module for non intel machines
+backlight() {
+	if [[ "$CARD" != *"intel_"* ]]; then
+		if [[ ! -f "$RFILE" ]]; then
+			sed -i -e 's/backlight/brightness/g' "$DIR"/config.ini
+			touch "$RFILE"
+		fi
+	fi
+}
 
 # Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+launch_bar() {
+	# Terminate already running bar instances
+	killall -q polybar
+
+	# Wait until the processes have been shut down
+	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+	# Launch the bar
+	polybar -q main -c "$DIR"/config.ini &
+}
+
+# Execute functions
+backlight
+launch_bar
