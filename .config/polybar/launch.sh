fac86927df04da6a46822314061322b8ce8b0acf
Matheus Vitor <m4theusdev@gmail.com>
2021-09-28 10:23:10 -0300

    :card_file_box: Separate modules in bars

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -24,7 +24,9 @@ launch_bar() {
 	while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 	# Launch the bar
-	polybar -q main -c "$DIR"/config.ini &
+	polybar -q left -c "$DIR"/config.ini &
+	polybar -q center -c "$DIR"/config.ini &
+	polybar -q right -c "$DIR"/config.ini &
 }
 
 # Execute functions
