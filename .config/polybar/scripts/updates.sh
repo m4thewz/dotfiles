a81eb792fcad8f796c0bbd7a1814cecd6b53720f
Matheus Vitor <m4theusdev@gmail.com>
2021-12-14 22:39:01 -0300

    :recycle: Updated config of MPD

--- .config/polybar/scripts/updates.sh
+++ .config/polybar/scripts/updates.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 if ! updates=$(checkupdates 2> /dev/null | wc -l ); then
-    updates=0
+  updates=0
 fi
 
 if [ "$updates" -gt 0 ]; then
-    echo $updates
+  echo $updates
 else
-    echo ""
+  echo ""
 fi
